b1f5357592782f7169521028293c5907b099a23f
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2017-07-24 01:30:08 -0400

    Wait at init

--- daemon.py
+++ daemon.py
@@ -4,6 +4,8 @@ import signal
 import BaseHTTPServer
 import urllib2
 import json
+import subprocess
+import time
 
 GLOBAL_TIMEOUT = int(os.getenv("GLOBAL_TIMEOUT") or 3600 * 4)
 UNSET_TIMEOUT = int(os.getenv("UNSET_TIMEOUT") or 30)
@@ -19,7 +21,14 @@ PROTECTED_IDS = set()
 
 def set_protection():
     print "Setting protection"
-    os.system("aws autoscaling set-instance-protection --instance-ids \"" + INSTANCE_ID + "\" --auto-scaling-group-name \"" + ASG_NAME + "\" --protected-from-scale-in")
+    for _ in range(12):
+        out = subprocess.check_output("aws autoscaling set-instance-protection --instance-ids \"" + INSTANCE_ID + "\" --auto-scaling-group-name \"" + ASG_NAME + "\" --protected-from-scale-in")
+
+        # The instance may still be starting up? Wait a bit more.
+        if "is not in InService " in out:
+            time.sleep(10)
+        else:
+            break
 
 
 def unset_protection():
@@ -37,9 +46,11 @@ signal.signal(signal.SIGALRM, sig_handler)
 
 class HTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
+        content = []
         if self.path.startswith("/setprotection/"):
             _id = self.path[15:]
             if len(PROTECTED_IDS) == 0:
+                content.append("Setting instance protection (timeout=%ss)" % GLOBAL_TIMEOUT)
                 signal.alarm(GLOBAL_TIMEOUT)
                 set_protection()
             PROTECTED_IDS.add(_id)
@@ -49,11 +60,16 @@ class HTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             if _id in PROTECTED_IDS:
                 PROTECTED_IDS.remove(_id)
                 if len(PROTECTED_IDS) == 0:
+                    content.append("Unsetting instance protection in %ss" % UNSET_TIMEOUT)
                     signal.alarm(UNSET_TIMEOUT)
 
+        content.append(json.dumps({"ids": list(PROTECTED_IDS)}))
+
         self.send_response(200)
-        self.wfile.write(json.dumps({"ids": list(PROTECTED_IDS)}))
+        self.send_header("Content-type", "text/plain")
         self.end_headers()
+        self.wfile.write("\n".join(content))
+
 
 port = int(os.getenv("PORT") or 29456)
 print "Server listening on %s" % port
